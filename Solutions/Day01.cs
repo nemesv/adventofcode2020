fbb2e8b8ce305201e137d31ef6c56abd39a9e6b3
Viktor Nemes <nemesv@gmail.com>
2020-12-01 06:23:25 +0100

    Solve Day01

--- Solutions/Day01.cs
+++ Solutions/Day01.cs
@@ -1,4 +1,7 @@
-﻿namespace Solutions
+﻿using System;
+using System.Linq;
+
+namespace Solutions
 {
     public class Day01 : Day
     {
@@ -8,12 +11,38 @@
 
         public override string Part1()
         {
-            throw new System.NotImplementedException();
+            var numbers = input.Split(Environment.NewLine).Select(int.Parse).ToList();
+            for (int i = 0; i < numbers.Count - 1; i++)
+            {
+                for (int j = i + 1; j < numbers.Count; j++)
+                {
+                    if (numbers[i] + numbers[j] == 2020)
+                        return (numbers[i] * numbers[j]).ToString();
+                }
+            }
+
+            return "";
         }
 
         public override string Part2()
         {
-            throw new System.NotImplementedException();
+            var numbers = input.Split(Environment.NewLine).Select(int.Parse).ToList();
+            for (int i = 0; i < numbers.Count - 2; i++)
+            {
+                for (int j = i + 1; j < numbers.Count - 1; j++)
+                {
+                    if (numbers[i] + numbers[j] > 2020)
+                        continue;
+
+                    for (int k = j + 1; k < numbers.Count; k++)
+                    {
+                        if (numbers[i] + numbers[j] + numbers[k] == 2020)
+                            return (numbers[i] * numbers[j] * numbers[k]).ToString();
+                    }
+                }
+            }
+
+            return "";
         }
     }
 }
--- Tests/Day01Tests.cs
+++ Tests/Day01Tests.cs
@@ -6,14 +6,24 @@ namespace Tests
 {
     public class Day01Tests : DayTest<Day01>
     {
-        [Input("input", "expected")]
+        [Input(@"1721
+            979
+            366
+            299
+            675
+            1456", "514579")]
         public void Part1(string input, string output)
         {
             var result = Sut(input.Replace(",", "\r\n")).Part1();
             result.ShouldBe(output);
         }
 
-        [Input("input", "expected")]
+        [Input(@"1721
+979
+366
+299
+675
+1456", "241861950")]
         public void Part2(string input, string output)
         {
             var result = Sut(input.Replace(",", "\r\n")).Part2();
