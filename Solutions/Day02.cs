d01345ffe89fdcf2b0af24a6668b1abfdcd2131c
Viktor Nemes <nemesv@gmail.com>
2020-12-02 06:35:09 +0100

    Solve Day02

--- Solutions/Day02.cs
+++ Solutions/Day02.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace Solutions
 {
@@ -11,12 +12,48 @@ namespace Solutions
 
         public override string Part1()
         {
-            throw new NotImplementedException();
+            var lines = input.Split(Environment.NewLine);
+            var result = 0;
+            foreach (var line in lines)
+            {
+                var parts = Regex.Match(line, @"(\d+)-(\d+) (\w): (.*)");
+                var min = int.Parse(parts.Groups[1].Value);
+                var max = int.Parse(parts.Groups[2].Value);
+                var letter = parts.Groups[3].Value;
+                var password = parts.Groups[4].Value;
+
+                var letterCount = password.Count(l => l.ToString() == letter);
+
+                if (min <= letterCount && letterCount <= max)
+                    result++;
+            }
+
+            return result.ToString();
         }
 
         public override string Part2()
         {
-            throw new NotImplementedException();
+            var lines = input.Split(Environment.NewLine);
+            var result = 0;
+            foreach (var line in lines)
+            {
+                var parts = Regex.Match(line, @"(\d+)-(\d+) (\w): (.*)");
+                var min = int.Parse(parts.Groups[1].Value);
+                var max = int.Parse(parts.Groups[2].Value);
+                var letter = parts.Groups[3].Value;
+                var password = parts.Groups[4].Value;
+
+                var matchCount = 0;
+                if (password[min - 1].ToString() == letter)
+                    matchCount++;
+                if (password[max - 1].ToString() == letter)
+                    matchCount++;
+
+                if (matchCount == 1)
+                    result++;
+            }
+
+            return result.ToString();
         }
     }
 }
--- Tests/Day02Tests.cs
+++ Tests/Day02Tests.cs
@@ -6,14 +6,18 @@ namespace Tests
 {
     public class Day02Tests : DayTest<Day02>
     {
-        [Input(@"input", "expected")]
+        [Input(@"1-3 a: abcde
+1-3 b: cdefg
+2-9 c: ccccccccc", "2")]
         public void Part1(string input, string output)
         {
             var result = Sut(input.Replace(",", "\r\n")).Part1();
             result.ShouldBe(output);
         }
 
-        [Input(@"input", "expected")]
+        [Input(@"1-3 a: abcde
+1-3 b: cdefg
+2-9 c: ccccccccc", "1")]
         public void Part2(string input, string output)
         {
             var result = Sut(input.Replace(",", "\r\n")).Part2();
