bafe7a077cfaf0ab973c53f639ee3fd9b9f681ff
Viktor Nemes <nemesv@gmail.com>
2020-12-07 06:58:21 +0100

    Solve Day07

--- Solutions/Day07.cs
+++ Solutions/Day07.cs
@@ -1,22 +1,120 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace Solutions
 {
     public class Day07 : Day
     {
+        public class Bag
+        {
+            public string Name { get; set; }
+
+            public List<(int, Bag)> Content { get; } = new List<(int, Bag)>();
+
+            public List<Bag> Parents { get; } = new List<Bag>();
+        }
+
         public Day07(string input) : base(input)
         {
         }
 
         public override string Part1()
         {
-            throw new NotImplementedException();
+            var bags = GetBags();
+
+            var shinyBag = bags["shiny gold"];
+            var containers = new HashSet<string>();
+            var parents = new Queue<Bag>(shinyBag.Parents);
+            while (parents.Count > 0)
+            {
+                var p = parents.Dequeue();
+                containers.Add(p.Name);
+                foreach (var pParent in p.Parents)
+                {
+                    parents.Enqueue(pParent);
+                }
+            }
+
+            return containers.Count.ToString();
         }
 
         public override string Part2()
         {
-            throw new NotImplementedException();
+            var bags = GetBags();
+
+            var shinyBag = bags["shiny gold"];
+            var r = 0;
+            foreach (var content in shinyBag.Content)
+            {
+                r += Count(content.Item2, content.Item1);
+            }
+
+            return r.ToString();
+        }
+
+        private Dictionary<string, Bag> GetBags()
+        {
+            var bags = new Dictionary<string, Bag>();
+            foreach (var line in input.Split(Environment.NewLine))
+            {
+                //light red bags contain 1 bright white bag, 2 muted yellow bags.
+                var parsts = line.Split(" contain ");
+
+                var bagName = parsts[0].Substring(0, parsts[0].Length - 5);
+
+                Bag bag;
+                if (bags.ContainsKey(bagName))
+                    bag = bags[bagName];
+                else
+                {
+                    bag = new Bag() { Name = bagName };
+                    bags.Add(bag.Name, bag);
+                }
+
+                if (parsts[1] != "no other bags.")
+                {
+
+                    var contents = parsts[1].Split(",");
+                    foreach (var content in contents)
+                    {
+                        var match = Regex.Match(content, @"\s*(\d) (.*) bag");
+                        bagName = match.Groups[2].Value;
+                        var bagCount = int.Parse(match.Groups[1].Value);
+                        if (bags.ContainsKey(bagName))
+                        {
+                            var bagToAdd = bags[bagName];
+                            bag.Content.Add((bagCount, bagToAdd));
+                            bagToAdd.Parents.Add(bag);
+                        }
+                        else
+                        {
+                            var bagToAdd = new Bag() { Name = bagName };
+                            bags.Add(bagToAdd.Name, bagToAdd);
+                            bag.Content.Add((bagCount, bagToAdd));
+                            bagToAdd.Parents.Add(bag);
+                        }
+                    }
+                }
+            }
+
+            return bags;
+        }
+
+        private int Count(Bag bag, int count)
+        {
+            var children = 1;
+            foreach (var child in bag.Content)
+            {
+                children += Count(child.Item2, child.Item1);
+            }
+
+            if (children == 0)
+                return count + 1;
+
+
+            return count * children;
         }
     }
 }
--- Tests/Day07Tests.cs
+++ Tests/Day07Tests.cs
@@ -6,17 +6,31 @@ namespace Tests
 {
     public class Day07Tests : DayTest<Day07>
     {
-        [Input(@"input", "expected")]
+        [Input(@"light red bags contain 1 bright white bag, 2 muted yellow bags.
+dark orange bags contain 3 bright white bags, 4 muted yellow bags.
+bright white bags contain 1 shiny gold bag.
+muted yellow bags contain 2 shiny gold bags, 9 faded blue bags.
+shiny gold bags contain 1 dark olive bag, 2 vibrant plum bags.
+dark olive bags contain 3 faded blue bags, 4 dotted black bags.
+vibrant plum bags contain 5 faded blue bags, 6 dotted black bags.
+faded blue bags contain no other bags.
+dotted black bags contain no other bags.", "4")]
         public void Part1(string input, string output)
         {
-            var result = Sut(input.Replace(",", "\r\n")).Part1();
+            var result = Sut(input).Part1();
             result.ShouldBe(output);
         }
 
-        [Input(@"input", "expected")]
+        [Input(@"shiny gold bags contain 2 dark red bags.
+dark red bags contain 2 dark orange bags.
+dark orange bags contain 2 dark yellow bags.
+dark yellow bags contain 2 dark green bags.
+dark green bags contain 2 dark blue bags.
+dark blue bags contain 2 dark violet bags.
+dark violet bags contain no other bags.", "126")]
         public void Part2(string input, string output)
         {
-            var result = Sut(input.Replace(",", "\r\n")).Part2();
+            var result = Sut(input.Replace(",", "")).Part2();
             result.ShouldBe(output);
         }
     }
