7ee591871a06149fff82b434205598b5d0992488
Viktor Nemes <nemesv@gmail.com>
2020-12-08 06:34:19 +0100

    Solve Day08

--- Solutions/Day08.cs
+++ Solutions/Day08.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace Solutions
@@ -11,12 +12,96 @@ namespace Solutions
 
         public override string Part1()
         {
-            throw new NotImplementedException();
+            var accumulator = 0;
+            var instructions = input
+                .Split(Environment.NewLine)
+                .Select(i =>
+                {
+                    var parts = i.Split(" ");
+                    return (Op: parts[0], Arg: int.Parse(parts[1]));
+                }).ToList();
+            var currentIndex = 0;
+            var visited = new HashSet<int>();
+
+            while (!visited.Contains(currentIndex))
+            {
+                var currentOp = instructions[currentIndex];
+                visited.Add(currentIndex);
+                switch (currentOp.Op)
+                {
+                    case "nop":
+                        currentIndex += 1;
+                        break;
+                    case "acc":
+                        accumulator += currentOp.Arg;
+                        currentIndex += 1;
+                        break;
+                    case "jmp":
+                        currentIndex += currentOp.Arg;
+                        break;
+                }
+            }
+            return accumulator.ToString();
         }
 
         public override string Part2()
         {
-            throw new NotImplementedException();
+            var instructions = input
+                .Split(Environment.NewLine)
+                .Select(i =>
+                {
+                    var parts = i.Split(" ");
+                    return (Op: parts[0], Arg: int.Parse(parts[1]));
+                }).ToList();
+
+            var changeAt = new HashSet<int>();
+
+            while (true)
+            {
+                var accumulator = 0;
+                var currentIndex = 0;
+                var visited = new HashSet<int>();
+                var changed = false;
+                while (!visited.Contains(currentIndex))
+                {
+                    var currentOp = instructions[currentIndex];
+
+                    if (!changed && !changeAt.Contains(currentIndex))
+                    {
+                        if (currentOp.Op == "nop")
+                        {
+                            currentOp = ("jmp", currentOp.Arg);
+                            changeAt.Add(currentIndex);
+                            changed = true;
+                        }
+                        else if (currentOp.Op == "jmp")
+                        {
+                            currentOp = ("nop", currentOp.Arg);
+                            changeAt.Add(currentIndex);
+                            changed = true;
+                        }
+                    }
+
+                    visited.Add(currentIndex);
+                    switch (currentOp.Op)
+                    {
+                        case "nop":
+                            currentIndex += 1;
+                            break;
+                        case "acc":
+                            accumulator += currentOp.Arg;
+                            currentIndex += 1;
+                            break;
+                        case "jmp":
+                            currentIndex += currentOp.Arg;
+                            break;
+                    }
+
+                    if (currentIndex >= instructions.Count)
+                        return accumulator.ToString();
+                }
+            }
+
         }
     }
 }
--- Tests/Day08Tests.cs
+++ Tests/Day08Tests.cs
@@ -6,14 +6,30 @@ namespace Tests
 {
     public class Day08Tests : DayTest<Day08>
     {
-        [Input(@"input", "expected")]
+        [Input(@"nop +0
+acc +1
+jmp +4
+acc +3
+jmp -3
+acc -99
+acc +1
+jmp -4
+acc +6", "5")]
         public void Part1(string input, string output)
         {
             var result = Sut(input.Replace(",", "\r\n")).Part1();
             result.ShouldBe(output);
         }
 
-        [Input(@"input", "expected")]
+        [Input(@"nop +0
+acc +1
+jmp +4
+acc +3
+jmp -3
+acc -99
+acc +1
+jmp -4
+acc +6", "8")]
         public void Part2(string input, string output)
         {
             var result = Sut(input.Replace(",", "\r\n")).Part2();
