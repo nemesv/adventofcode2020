bff646fdfb3f415d51c23791e6c4e8042989cd30
Viktor Nemes <nemesv@gmail.com>
2020-12-09 06:36:19 +0100

    Solve Day09

--- Solutions/Day09.cs
+++ Solutions/Day09.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace Solutions
@@ -11,12 +12,68 @@ namespace Solutions
 
         public override string Part1()
         {
-            throw new NotImplementedException();
+            return Part1(25);
+        }
+
+        public string Part1(int preamble)
+        {
+            var numbers = input.Split(Environment.NewLine).Select(long.Parse).ToArray();
+            var i = 0;
+            foreach (var n in numbers.Skip(preamble))
+            {
+                if (!CheckSum(numbers.Skip(i).Take(preamble).ToArray(), n))
+                    return n.ToString();
+                i++;
+            }
+
+            return "";
+        }
+
+        private bool CheckSum(long[] numbers, long n)
+        {
+            var current = numbers;
+            for (int j = 0; j < current.Length; j++)
+            {
+                for (int k = j; k < current.Length; k++)
+                {
+                    if (current[j] + current[k] == n)
+                    {
+                        return true;
+                    }
+                }
+            }
+
+            return false;
         }
 
         public override string Part2()
         {
-            throw new NotImplementedException();
+            return Part2(144381670);
+        }
+
+        public string Part2(int target)
+        {
+            var numbers = input.Split(Environment.NewLine).Select(long.Parse).ToArray();
+            
+            var current = new List<long>() { numbers[0], numbers[1]};
+            var currentIndex = 2;
+            while (true)
+            {
+                var currentSum = current.Sum();
+                if (current.Count > 1 && currentSum == target)
+                    break;
+                if (currentSum < target)
+                {
+                    current.Add(numbers[currentIndex]);
+                    currentIndex++;
+                }
+                else
+                {
+                    current.RemoveAt(0);
+                }
+            }
+
+            return (current.Max() + current.Min()).ToString();
         }
     }
 }
--- Tests/Day09Tests.cs
+++ Tests/Day09Tests.cs
@@ -6,17 +6,55 @@ namespace Tests
 {
     public class Day09Tests : DayTest<Day09>
     {
-        [Input(@"input", "expected")]
+        [Input(@"35
+20
+15
+25
+47
+40
+62
+55
+65
+95
+102
+117
+150
+182
+127
+219
+299
+277
+309
+576", "127")]
         public void Part1(string input, string output)
         {
-            var result = Sut(input.Replace(",", "\r\n")).Part1();
+            var result = Sut(input.Replace(",", "\r\n")).Part1(5);
             result.ShouldBe(output);
         }
 
-        [Input(@"input", "expected")]
+        [Input(@"35
+20
+15
+25
+47
+40
+62
+55
+65
+95
+102
+117
+150
+182
+127
+219
+299
+277
+309
+576", "62")]
         public void Part2(string input, string output)
         {
-            var result = Sut(input.Replace(",", "\r\n")).Part2();
+            var result = Sut(input.Replace(",", "\r\n")).Part2(127);
             result.ShouldBe(output);
         }
     }
