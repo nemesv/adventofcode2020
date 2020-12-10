691e4c710b518e2b2295091569c7993c8c8fc067
Viktor Nemes <nemesv@gmail.com>
2020-12-10 20:04:34 +0100

    Solve Day10

--- Solutions/Day10.cs
+++ Solutions/Day10.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace Solutions
@@ -11,12 +12,66 @@ namespace Solutions
 
         public override string Part1()
         {
-            throw new NotImplementedException();
+            var adapters = input.Split(Environment.NewLine).Select(int.Parse)
+                .OrderBy(a => a).ToList();
+            adapters.Insert(0, 0);
+            adapters.Add(adapters.Max() + 3);
+            var ones = 0;
+            var threes = 0;
+            var twos = 0;
+            for (int i = 0; i < adapters.Count - 1; i++)
+            {
+                var difference = adapters[i + 1] - adapters[i];
+                if (difference == 1)
+                    ones++;
+                if (difference == 3)
+                    threes++;
+                if (difference == 2)
+                    twos++;
+            }
+            return (ones * threes).ToString();
         }
 
         public override string Part2()
         {
-            throw new NotImplementedException();
+            var adapters = input.Split(Environment.NewLine).Select(int.Parse)
+                .OrderBy(a => a).ToList();
+            adapters.Insert(0, 0);
+            adapters.Add(adapters.Max() + 3);
+            var oneLengths = new List<int>();
+            var c = 0;
+            for (int i = 0; i < adapters.Count - 1; i++)
+            {
+                var difference = adapters[i + 1] - adapters[i];
+                if (difference == 1)
+                {
+                    c++;
+                }
+                else
+                {
+                    oneLengths.Add(c);
+                    c = 0;
+                }
+            }
+
+            var result = 1L;
+            foreach (var oneLength in oneLengths)
+            {
+                switch (oneLength)
+                {
+                    case 2:
+                        result *= 2;
+                        break;
+                    case 3:
+                        result *= 4;
+                        break;
+                    case 4:
+                        result *= 7;
+                        break;
+                }
+            }
+
+            return result.ToString();
         }
     }
 }
--- Tests/Day10Tests.cs
+++ Tests/Day10Tests.cs
@@ -6,14 +6,85 @@ namespace Tests
 {
     public class Day10Tests : DayTest<Day10>
     {
-        [Input(@"input", "expected")]
+        [Input(@"28
+33
+18
+42
+31
+14
+46
+20
+48
+47
+24
+23
+49
+45
+19
+38
+39
+11
+1
+32
+25
+35
+8
+17
+7
+9
+4
+2
+34
+10
+3", "220")]
         public void Part1(string input, string output)
         {
             var result = Sut(input.Replace(",", "\r\n")).Part1();
             result.ShouldBe(output);
         }
 
-        [Input(@"input", "expected")]
+        [Input(@"16
+10
+15
+5
+1
+11
+7
+19
+6
+12
+4", "8")]
+        [Input(@"28
+33
+18
+42
+31
+14
+46
+20
+48
+47
+24
+23
+49
+45
+19
+38
+39
+11
+1
+32
+25
+35
+8
+17
+7
+9
+4
+2
+34
+10
+3", "19208")]
         public void Part2(string input, string output)
         {
             var result = Sut(input.Replace(",", "\r\n")).Part2();
