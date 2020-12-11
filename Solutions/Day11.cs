64be9378201b62ccc7b402225866407bd4ca984c
Viktor Nemes <nemesv@gmail.com>
2020-12-11 06:46:04 +0100

    Solve Day11

--- Solutions/Day11.cs
+++ Solutions/Day11.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace Solutions
@@ -11,12 +12,171 @@ namespace Solutions
 
         public override string Part1()
         {
-            throw new NotImplementedException();
+            var currentLayout = input.Split(Environment.NewLine)
+                .Select(s => s.Select(c => c.ToString()).ToList()).ToList();
+
+            var iterations = 0;
+            while (true)
+            {
+                var newLayout = new List<List<string>>();
+
+                for (int rowIndex = 0; rowIndex < currentLayout.Count; rowIndex++)
+                {
+                    newLayout.Add(new List<string>());
+                    for (int columnIndex = 0; columnIndex < currentLayout[0].Count; columnIndex++)
+                    {
+                        var current = currentLayout[rowIndex][columnIndex];
+
+                        if (current == ".")
+                        {
+                            newLayout[rowIndex].Add(".");
+                            continue;
+                        }
+
+                        var occupied = GetNeighbors(currentLayout, rowIndex, columnIndex)
+                            .Count(n => n == "#");
+
+                        if (current == "L")
+                        {
+                            newLayout[rowIndex].Add(current == "L" && occupied == 0 ? "#" : "L");
+                            continue;
+                        }
+
+                        newLayout[rowIndex].Add(current == "#" && occupied >= 4 ? "L" : "#");
+                    }
+                }
+
+                iterations++;
+
+                if (LayoutToString(currentLayout) == LayoutToString(newLayout))
+                    break;
+
+                currentLayout = newLayout;
+            }
+
+            return currentLayout.Sum(r => r.Count(s => s == "#")).ToString();
+        }
+
+        private List<string> GetNeighbors(
+            List<List<string>> layout, int row, int column)
+        {
+            var result = new List<string>();
+
+            var offsets = new int[] {-1, 0, 1};
+            foreach (var rowOffset in offsets)
+            {
+                foreach (var columnOffset in offsets)
+                {
+                    if (rowOffset == 0 && columnOffset == 0)
+                        continue;
+
+                    var cRow = row + rowOffset;
+                    var cCloumn = column + columnOffset;
+
+                    if (cRow > -1 && cRow < layout.Count)
+                    {
+                        if (cCloumn > -1 && cCloumn < layout[cRow].Count)
+                        {
+                            result.Add(layout[cRow][cCloumn]);
+                        }
+                    }
+                }
+            }
+ 
+            return result;
+        }
+
+        private string LayoutToString(IEnumerable<IEnumerable<string>> layout)
+        {
+            return String.Join("", layout.Select(s => string.Join("",s)));
         }
 
         public override string Part2()
         {
-            throw new NotImplementedException();
+            var currentLayout = input.Split(Environment.NewLine)
+                .Select(s => s.Select(c => c.ToString()).ToList()).ToList();
+
+            var iterations = 0;
+            while (true)
+            {
+                var newLayout = new List<List<string>>();
+
+                for (int rowIndex = 0; rowIndex < currentLayout.Count; rowIndex++)
+                {
+                    newLayout.Add(new List<string>());
+                    for (int columnIndex = 0; columnIndex < currentLayout[0].Count; columnIndex++)
+                    {
+                        var current = currentLayout[rowIndex][columnIndex];
+
+                        if (current == ".")
+                        {
+                            newLayout[rowIndex].Add(".");
+                            continue;
+                        }
+
+                        var occupied = GetOccupied(currentLayout, rowIndex, columnIndex)
+                            .Count(n => n == "#");
+
+                        if (current == "L")
+                        {
+                            newLayout[rowIndex].Add(current == "L" && occupied == 0 ? "#" : "L");
+                            continue;
+                        }
+
+                        newLayout[rowIndex].Add(current == "#" && occupied >= 5 ? "L" : "#");
+                    }
+                }
+
+                iterations++;
+
+                if (LayoutToString(currentLayout) == LayoutToString(newLayout))
+                    break;
+
+                currentLayout = newLayout;
+            }
+
+            return currentLayout.Sum(r => r.Count(s => s == "#")).ToString();
+        }
+
+        private List<string> GetOccupied(
+            List<List<string>> layout, int row, int column)
+        {
+            var result = new List<string>();
+
+            var offsets = new int[] { -1, 0, 1 };
+            foreach (var rowOffset in offsets)
+            {
+                foreach (var columnOffset in offsets)
+                {
+                    if (rowOffset == 0 && columnOffset == 0)
+                        continue;
+
+                    var cRow = row + rowOffset;
+                    var cCloumn = column + columnOffset;
+
+
+                    while (true)
+                    {
+                        if (cRow > -1 && cRow < layout.Count)
+                        {
+                            if (cCloumn > -1 && cCloumn < layout[cRow].Count)
+                            {
+                                var current = layout[cRow][cCloumn];
+                                if (current == ".")
+                                {
+                                    cRow += rowOffset;
+                                    cCloumn += columnOffset;
+                                    continue;
+                                }
+                                result.Add(current);
+                            }
+                        }
+                        break;
+                    }
+                }
+            }
+
+            return result;
         }
     }
 }
--- Tests/Day11Tests.cs
+++ Tests/Day11Tests.cs
@@ -6,14 +6,32 @@ namespace Tests
 {
     public class Day11Tests : DayTest<Day11>
     {
-        [Input(@"input", "expected")]
+        [Input(@"L.LL.LL.LL
+LLLLLLL.LL
+L.L.L..L..
+LLLL.LL.LL
+L.LL.LL.LL
+L.LLLLL.LL
+..L.L.....
+LLLLLLLLLL
+L.LLLLLL.L
+L.LLLLL.LL", "37")]
         public void Part1(string input, string output)
         {
             var result = Sut(input.Replace(",", "\r\n")).Part1();
             result.ShouldBe(output);
         }
 
-        [Input(@"input", "expected")]
+        [Input(@"L.LL.LL.LL
+LLLLLLL.LL
+L.L.L..L..
+LLLL.LL.LL
+L.LL.LL.LL
+L.LLLLL.LL
+..L.L.....
+LLLLLLLLLL
+L.LLLLLL.L
+L.LLLLL.LL", "26")]
         public void Part2(string input, string output)
         {
             var result = Sut(input.Replace(",", "\r\n")).Part2();
