b7c52097e6ad241b5ab42f0d8692e534e400e7fa
Viktor Nemes <nemesv@gmail.com>
2020-12-12 08:16:48 +0100

    Solve Day12

--- Solutions/Day12.cs
+++ Solutions/Day12.cs
@@ -11,12 +11,104 @@ namespace Solutions
 
         public override string Part1()
         {
-            throw new NotImplementedException();
+            var actions = input.Split(Environment.NewLine)
+                .Select(a => (a[0], int.Parse(a[1..]))).ToArray();
+            var position = (0, 0);
+            var direction = 'E';
+            var directions = new[] {'N', 'E', 'S', 'W'};
+
+            foreach (var action in actions)
+            {
+                switch (action.Item1)
+                {
+                    case 'N':
+                        position = (position.Item1 + action.Item2, position.Item2);
+                        break;
+                    case 'S':
+                        position = (position.Item1 - action.Item2, position.Item2);
+                        break;
+                    case 'E':
+                        position = (position.Item1, position.Item2 + action.Item2);
+                        break;
+                    case 'W':
+                        position = (position.Item1, position.Item2 - action.Item2);
+                        break;
+                    case 'L':
+                        direction = directions[(Array.IndexOf(directions, direction) + 4 - action.Item2 / 90) % 4];
+                        break;
+                    case 'R':
+                        direction = directions[(Array.IndexOf(directions, direction) + action.Item2 / 90) % 4];
+
+                        break;
+                    case 'F':
+
+                        switch (direction)
+                        {
+                            case 'N':
+                                position = (position.Item1 + action.Item2, position.Item2);
+                                break;
+                            case 'S':
+                                position = (position.Item1 - action.Item2, position.Item2);
+                                break;
+                            case 'E':
+                                position = (position.Item1, position.Item2 + action.Item2);
+                                break;
+                            case 'W':
+                                position = (position.Item1, position.Item2 - action.Item2);
+                                break;
+                        }
+
+                        break;
+                }
+            }
+
+            return (Math.Abs(position.Item1) + Math.Abs(position.Item2)).ToString();
         }
 
         public override string Part2()
         {
-            throw new NotImplementedException();
+            var actions = input.Split(Environment.NewLine)
+                .Select(a => (a[0], int.Parse(a[1..]))).ToArray();
+            var position = (0, 0);
+            var waypoint = (1, 10);
+
+            foreach (var action in actions)
+            {
+                switch (action.Item1)
+                {
+                    case 'N':
+                        waypoint = (waypoint.Item1 + action.Item2, waypoint.Item2);
+                        break;
+                    case 'S':
+                        waypoint = (waypoint.Item1 - action.Item2, waypoint.Item2);
+                        break;
+                    case 'E':
+                        waypoint = (waypoint.Item1, waypoint.Item2 + action.Item2);
+                        break;
+                    case 'W':
+                        waypoint = (waypoint.Item1, waypoint.Item2 - action.Item2);
+                        break; 
+                    //https://math.stackexchange.com/questions/1330161/how-to-rotate-points-through-90-degree
+                    case 'R':
+                        for (int i = 0; i < action.Item2 / 90; i++)
+                        {
+                            waypoint = (-waypoint.Item2, waypoint.Item1);
+                        }
+                        break;
+                    case 'L':
+                        for (int i = 0; i < action.Item2 / 90; i++)
+                        {
+                            waypoint = (waypoint.Item2, -waypoint.Item1);
+                        }
+                        break;
+                    case 'F':
+                        position = (position.Item1 + waypoint.Item1 * action.Item2,
+                            position.Item2 + waypoint.Item2 * action.Item2);
+                        break;
+                }
+            }
+
+            return (Math.Abs(position.Item1) + Math.Abs(position.Item2)).ToString();
         }
     }
 }
--- Tests/Day12Tests.cs
+++ Tests/Day12Tests.cs
@@ -6,14 +6,22 @@ namespace Tests
 {
     public class Day12Tests : DayTest<Day12>
     {
-        [Input(@"input", "expected")]
+        [Input(@"F10
+N3
+F7
+R90
+F11", "25")]
         public void Part1(string input, string output)
         {
             var result = Sut(input.Replace(",", "\r\n")).Part1();
             result.ShouldBe(output);
         }
 
-        [Input(@"input", "expected")]
+        [Input(@"F10
+N3
+F7
+R90
+F11", "286")]
         public void Part2(string input, string output)
         {
             var result = Sut(input.Replace(",", "\r\n")).Part2();
