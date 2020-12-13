eb6eef78f37fa69cd035e4c2d2dcb14cb794317a
Viktor Nemes <nemesv@gmail.com>
2020-12-13 09:07:41 +0100

    Solve Day13

--- Solutions/Day13.cs
+++ Solutions/Day13.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace Solutions
@@ -11,12 +12,74 @@ namespace Solutions
 
         public override string Part1()
         {
-            throw new NotImplementedException();
+            var lines = input.Split(Environment.NewLine);
+            var departTime = int.Parse(lines[0]);
+            var buses = lines[1].Split(",").Where(b => b != "x").Select(int.Parse).ToArray();
+
+            var waitTimes = new List<(int Bus, int Wait)>();
+
+            foreach (var bus in buses)
+            {
+                var d = departTime / bus;
+                waitTimes.Add((bus, (d + 1) * bus - departTime));
+            }
+
+            var result = waitTimes.OrderBy(w => w.Wait).First();
+            return (result.Wait * result.Bus).ToString();
         }
 
         public override string Part2()
         {
-            throw new NotImplementedException();
+            var lines = input.Split(Environment.NewLine);
+            var departTime = int.Parse(lines[0]);
+            var buses = lines[1].Split(",")
+                .Select((b,i) =>
+                {
+                    if (b == "x")
+                        return 0;
+
+                    return int.Parse(b);
+                })
+                .ToArray();
+
+
+            return ChineseRemainderTheorem
+                .Solve(
+                    buses.Where(b => b !=0).ToArray(),
+                    buses.Where(b => b != 0)
+                        .Select(b => Array.IndexOf(buses, b) - b).ToArray()
+                )
+                .ToString();
+        }
+
+        //https://rosettacode.org/wiki/Chinese_remainder_theorem#C.23
+        public static class ChineseRemainderTheorem
+        {
+            public static long Solve(int[] n, int[] a)
+            {
+                long prod = n.Aggregate(1L, (i, j) => i * j);
+                long p;
+                long sm = 0;
+                for (long i = 0; i < n.Length; i++)
+                {
+                    p = prod / n[i];
+                    sm += a[i] * ModularMultiplicativeInverse(p, n[i]) * p;
+                }
+                return Math.Abs(sm % prod);
+            }
+
+            private static long ModularMultiplicativeInverse(long a, long mod)
+            {
+                long b = a % mod;
+                for (long x = 1; x < mod; x++)
+                {
+                    if ((b * x) % mod == 1)
+                    {
+                        return x;
+                    }
+                }
+                return 1;
+            }
         }
     }
 }
--- Tests/Day13Tests.cs
+++ Tests/Day13Tests.cs
@@ -6,17 +6,25 @@ namespace Tests
 {
     public class Day13Tests : DayTest<Day13>
     {
-        [Input(@"input", "expected")]
+        [Input(@"939
+7,13,x,x,59,x,31,19", "295")]
         public void Part1(string input, string output)
         {
-            var result = Sut(input.Replace(",", "\r\n")).Part1();
+            var result = Sut(input).Part1();
             result.ShouldBe(output);
         }
 
-        [Input(@"input", "expected")]
+        [Input(@"939
+17,x,13,19", "3417")]
+        [Input(@"939
+67,7,59,61", "754018")]
+        [Input(@"939
+67,x,7,59,61", "779210")]
+        [Input(@"939
+1789,37,47,1889", "1202161486")]
         public void Part2(string input, string output)
         {
-            var result = Sut(input.Replace(",", "\r\n")).Part2();
+            var result = Sut(input).Part2();
             result.ShouldBe(output);
         }
     }
