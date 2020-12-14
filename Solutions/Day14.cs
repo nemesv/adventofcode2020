58eb99a65b414a8d86259940a82876300b3d7ee7
Viktor Nemes <nemesv@gmail.com>
2020-12-14 18:43:13 +0100

    Solve Day14

--- Solutions/Day14.cs
+++ Solutions/Day14.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace Solutions
 {
@@ -11,12 +13,111 @@ namespace Solutions
 
         public override string Part1()
         {
-            throw new NotImplementedException();
+            var lines = input.Split(Environment.NewLine);
+            var currentMask = new List<char>();
+            var mem = new Dictionary<long, long>();
+            foreach (var line in lines)
+            {
+                if (line.StartsWith("mask"))
+                {
+                    currentMask = line.Substring(7).ToList();
+                }
+                else
+                {
+                    var parts = Regex.Match(line, @"mem\[(\d+)\] = (\d+)");
+                    var address = long.Parse(parts.Groups[1].Value);
+                    var value = long.Parse(parts.Groups[2].Value);
+
+                    var valueInBits = Convert.ToString(value, 2).PadLeft(36, '0');
+                    var result = new List<char>();
+                    for (int i = 0; i < currentMask.Count; i++)
+                    {
+                        var maskBit = currentMask[i];
+                        var valueBit = valueInBits[i];
+                        if (maskBit == 'X')
+                        {
+                            result.Add(valueBit);
+                        }
+                        else
+                        {
+                            result.Add(maskBit);
+                        }
+                    }
+
+                    var newValue = Convert.ToInt64(string.Join("", result), 2);
+                    mem[address] = newValue;
+                }
+            }
+
+            return mem.Values.Sum().ToString();
         }
 
         public override string Part2()
         {
-            throw new NotImplementedException();
+            var lines = input.Split(Environment.NewLine);
+            var currentMask = new List<char>();
+            var mem = new Dictionary<long, long>();
+            foreach (var line in lines)
+            {
+                if (line.StartsWith("mask"))
+                {
+                    currentMask = line.Substring(7).ToList();
+                }
+                else
+                {
+                    var parts = Regex.Match(line, @"mem\[(\d+)\] = (\d+)");
+                    var address = long.Parse(parts.Groups[1].Value);
+                    var value = long.Parse(parts.Groups[2].Value);
+
+                    var addressInBits = Convert.ToString(address, 2).PadLeft(36, '0');
+                    var result = new List<char>();
+                    for (int i = 0; i < currentMask.Count; i++)
+                    {
+                        var maskBit = currentMask[i];
+                        var addressBit = addressInBits[i];
+                        if (maskBit == '0')
+                        {
+                            result.Add(addressBit);
+                        }
+                        else
+                        {
+                            result.Add(maskBit);
+                        }
+                    }
+
+                 
+                    var newAddresses = new Queue<List<char>>();
+                    newAddresses.Enqueue(result);
+
+                    while (true)
+                    {
+                        var current = newAddresses.Dequeue();
+
+                        var firstFloating = current.IndexOf('X');
+                        if (firstFloating == -1)
+                        {
+                            newAddresses.Enqueue(current);
+                            break;
+                        }
+
+                        var l1 = current.ToList();
+                        var l2 = current.ToList();
+                        l1[firstFloating] = '0';
+                        l2[firstFloating] = '1';
+
+                        newAddresses.Enqueue(l1);
+                        newAddresses.Enqueue(l2);
+                    }
+
+                    foreach (var newAddressA in newAddresses)
+                    {
+                        var newAddress = Convert.ToInt64(string.Join("", newAddressA), 2);
+                        mem[newAddress] = value;
+                    }
+                }
+            }
+
+            return mem.Values.Sum().ToString();
         }
     }
 }
--- Tests/Day14Tests.cs
+++ Tests/Day14Tests.cs
@@ -6,14 +6,20 @@ namespace Tests
 {
     public class Day14Tests : DayTest<Day14>
     {
-        [Input(@"input", "expected")]
+        [Input(@"mask = XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X
+mem[8] = 11
+mem[7] = 101
+mem[8] = 0", "165")]
         public void Part1(string input, string output)
         {
             var result = Sut(input.Replace(",", "\r\n")).Part1();
             result.ShouldBe(output);
         }
 
-        [Input(@"input", "expected")]
+        [Input(@"mask = 000000000000000000000000000000X1001X
+mem[42] = 100
+mask = 00000000000000000000000000000000X0XX
+mem[26] = 1", "208")]
         public void Part2(string input, string output)
         {
             var result = Sut(input.Replace(",", "\r\n")).Part2();
