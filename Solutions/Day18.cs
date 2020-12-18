b058922560a964af7c5d49ab5f4213ebcb5057c9
Viktor Nemes <nemesv@gmail.com>
2020-12-18 07:12:23 +0100

    Solve Day18

--- Solutions/Day18.cs
+++ Solutions/Day18.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 
 namespace Solutions
 {
@@ -11,12 +13,131 @@ namespace Solutions
 
         public override string Part1()
         {
-            throw new NotImplementedException();
+            var expressions = input.Split(Environment.NewLine);
+            var result = 0L;
+
+            bool HasPrecedence(char op1, char op2)
+            {
+                if (op2 == '(' || op2 == ')')
+                {
+                    return false;
+                }
+                if ((op1 == '*' || op1 == '+'))
+                {
+                    return true;
+                }
+                return true;
+            }
+
+            foreach (var expression in expressions)
+            {
+                result += Evaluate(expression, HasPrecedence);
+            }
+
+            return result.ToString();
+        }
+
+        private long Evaluate(string expression, Func<char, char, bool> hasPrecedence)
+        {
+            long Apply(char op, long arg1, long arg2)
+            {
+                switch (op)
+                {
+                    case '+':
+                        return arg1 + arg2;
+                    case '*':
+                        return arg1 * arg2;
+                    default:
+                        throw new NotSupportedException();
+                }
+
+            }
+
+            var tokens = expression.ToArray();
+            var opStack = new Stack<char>();
+            var values = new Stack<long>();
+            for (int i = 0; i < tokens.Length; i++)
+            {
+                var token = tokens[i];
+                switch (token)
+                {
+                    case ' ':
+                        break;
+                    case '+':
+                    case '*':
+                        while (opStack.Count > 0 &&
+                               hasPrecedence(token,
+                                   opStack.Peek()))
+                        {
+                            values.Push(Apply(opStack.Pop(),
+                                values.Pop(),
+                                values.Pop()));
+                        }
+
+                        opStack.Push(token);
+                        break;
+                    case '(':
+                        opStack.Push(token);
+                        break;
+                    case ')':
+                        while (opStack.Peek() != '(')
+                        {
+                            values.Push(Apply(opStack.Pop(),
+                                values.Pop(),
+                                values.Pop()));
+                        }
+                        opStack.Pop();
+                        break;
+                    default:
+                        StringBuilder builder = new StringBuilder();
+                        while (i < tokens.Length &&
+                               tokens[i] >= '0' &&
+                               tokens[i] <= '9')
+                        {
+                            builder.Append(tokens[i++]);
+                        }
+                        values.Push(int.Parse(builder.ToString()));
+                        i--;
+                        break;
+                }
+            }
+
+            while (opStack.Count > 0)
+            {
+                values.Push(Apply(opStack.Pop(),
+                    values.Pop(),
+                    values.Pop()));
+            }
+
+            return  values.Pop();
         }
 
         public override string Part2()
         {
-            throw new NotImplementedException();
+            var expressions = input.Split(Environment.NewLine);
+            var result = 0L;
+
+            bool HasPrecedence(char op1, char op2)
+            {
+                if (op2 == '(' || op2 == ')')
+                {
+                    return false;
+                }
+
+                if (op2 == '*' && op1 == '+')
+                {
+                    return false;
+                }
+
+                return true;
+            }
+
+            foreach (var expression in expressions)
+            {
+                result += Evaluate(expression, HasPrecedence);
+            }
+
+            return result.ToString();
         }
     }
 }
--- Tests/Day18Tests.cs
+++ Tests/Day18Tests.cs
@@ -6,14 +6,15 @@ namespace Tests
 {
     public class Day18Tests : DayTest<Day18>
     {
-        [Input(@"input", "expected")]
+        [Input(@"2 * 3 + (4 * 5)
+((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2", "13658")]
         public void Part1(string input, string output)
         {
             var result = Sut(input.Replace(",", "\r\n")).Part1();
             result.ShouldBe(output);
         }
 
-        [Input(@"input", "expected")]
+        [Input(@"((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2", "23340")]
         public void Part2(string input, string output)
         {
             var result = Sut(input.Replace(",", "\r\n")).Part2();
