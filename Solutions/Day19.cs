05d6f298ae71dcbe17bef59793b7353ed5cb31ad
Viktor Nemes <nemesv@gmail.com>
2020-12-19 11:36:46 +0100

    Solve Day19 Part2

--- Solutions/Day19.cs
+++ Solutions/Day19.cs
@@ -59,7 +59,7 @@ namespace Solutions
             {
                 if (rule.Value[0][0] == "a" || rule.Value[0][0] == "b")
                 {
-                    resolved[rule.Key] = new List<string>() {rule.Value[0][0]};
+                    resolved[rule.Key] = new List<string>() { rule.Value[0][0] };
                 }
             }
             while (!resolved.ContainsKey("0"))
@@ -70,7 +70,7 @@ namespace Solutions
                         continue;
 
                     var canResolve = true;
-                    var r = new List<string>() {};
+                    var r = new List<string>() { };
                     foreach (var option in parsedRule.Value)
                     {
                         var partResult = new List<string>();
@@ -115,7 +115,123 @@ namespace Solutions
 
         public override string Part2()
         {
-            throw new NotImplementedException();
+            var lines = input.Split(Environment.NewLine);
+            var rules = lines.TakeWhile(l => l.Length > 0).ToList();
+            var messages = lines.SkipWhile(l => l.Length > 0).Skip(1).ToList();
+            var parsedRules = new Dictionary<string, List<List<string>>>();
+            foreach (var rule in rules)
+            {
+                var mainParts = rule.Split(":");
+                var id = mainParts[0];
+
+                if (rule.Contains('"'))
+                {
+                    parsedRules[id] = new List<List<string>> { new List<string>() { mainParts[1].Trim().Trim('"') } };
+                }
+                else
+                {
+                    var details = mainParts[1].Trim().Split("|");
+                    var options = new List<List<string>>();
+                    foreach (var detail in details)
+                    {
+                        options.Add(detail.Split(' ', StringSplitOptions.RemoveEmptyEntries).ToList());
+                    }
+
+                    parsedRules[id] = options;
+                }
+            }
+            
+            var resolved = new Dictionary<string, List<string>>();
+            foreach (var rule in parsedRules)
+            {
+                if (rule.Value[0][0] == "a" || rule.Value[0][0] == "b")
+                {
+                    resolved[rule.Key] = new List<string>() { rule.Value[0][0] };
+                }
+            }
+
+            var maxMessageLength = messages.Max(m => m.Length);
+            while (!resolved.ContainsKey("0"))
+            {
+                foreach (var parsedRule in parsedRules)
+                {
+                    if (resolved.ContainsKey(parsedRule.Key))
+                        continue;
+
+                    var canResolve = true;
+                    var r = new List<string>() { };
+                    foreach (var option in parsedRule.Value)
+                    {
+                        var partResult = new List<string>();
+                        foreach (var value in option)
+                        {
+                            if (!resolved.ContainsKey(value))
+                            {
+                                canResolve = false;
+                                break;
+                            }
+
+                            if (partResult.Count == 0)
+                            {
+                                partResult.AddRange(resolved[value]);
+                            }
+                            else
+                            {
+                                var n = new List<string>();
+                                foreach (var res in resolved[value])
+                                {
+                                    n.AddRange(partResult.Select(p => p + res).Where(s => s.Length <= maxMessageLength));
+                                }
+
+                                partResult = n;
+                            }
+                        }
+                        if (!canResolve)
+                            break;
+                        r.AddRange(partResult);
+                    }
+
+                    if (canResolve)
+                    {
+                            resolved[parsedRule.Key] = r;
+                    }
+                }
+            }
+
+            var result = 0;
+            var patternLength = resolved["8"][0].Length;
+            foreach (var message in messages)
+            {
+                if (message.Length % patternLength != 0)
+                    continue;
+
+                var patterns = new List<int>();
+                for (int i = 0; i < message.Length / patternLength; i++)
+                {
+                    //0 7 15
+                    var part = message.Substring(i*patternLength, patternLength);
+                    if (resolved["42"].Contains(part))
+                        patterns.Add(42);
+                    else if (resolved["31"].Contains(part))
+                        patterns.Add(31);
+                    else
+                    {
+                        patterns.Clear();
+                        break;
+                    }
+                }
+
+                if (patterns[0] == 42 && patterns.Last() == 31)
+                {
+                    var firstPart = patterns.TakeWhile(p => p == 42).ToList();
+                    var secondPart = patterns.SkipWhile(p => p == 42).ToList();
+                    
+                    if (firstPart.Count > secondPart.Count && secondPart.All(p => p == 31))
+                        result++;
+                }
+            }
+            
+            return result.ToString();
         }
     }
 }
