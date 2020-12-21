f4fdc4e0e49a9f6bf45a715c7eb1bfafc43a11c7
Viktor Nemes <nemesv@gmail.com>
2020-12-21 08:07:29 +0100

    Solve Day21

--- Solutions/Day21.cs
+++ Solutions/Day21.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace Solutions
 {
@@ -9,14 +11,122 @@ namespace Solutions
         {
         }
 
+        public record Food(HashSet<string> Allergens, HashSet<string> Ingredients);
+
         public override string Part1()
         {
-            throw new NotImplementedException();
+            var lines = input.Split(Environment.NewLine);
+            var foods = new List<Food>();
+            var allIngredients = new Dictionary<string, string>();
+            var allAllergens = new Dictionary<string, List<Food>>();
+            foreach (var line in lines)
+            {
+                var parts = Regex.Match(line, "(.*) \\(contains (.*)\\)");
+                var ingredients = parts.Groups[1].Value.Split(" ");
+                var allergens = parts.Groups[2].Value.Split(", ");
+                foreach (var ingredient in ingredients)
+                {
+                    allIngredients[ingredient] = null;
+                }
+
+                var food = new Food(new HashSet<string>(allergens), new HashSet<string>(ingredients));
+                foreach (var allergen in allergens)
+                {
+                    if (allAllergens.ContainsKey(allergen))
+                        allAllergens[allergen].Add(food);
+                    else
+                        allAllergens[allergen] = new List<Food>() { food };
+                }
+                foods.Add(food);
+            }
+
+            foreach (var allergen in allAllergens.OrderByDescending(k => k.Value.Count))
+            {
+                var foodsWithAllergen = allergen.Value;
+                var firstIngredients =
+                    new HashSet<string>(foodsWithAllergen[0].Ingredients.Where(i => allIngredients[i] == null));
+                foreach (var food in foodsWithAllergen.Skip(1))
+                {
+                    firstIngredients.IntersectWith(food.Ingredients.Where(i => allIngredients[i] == null));
+                }
+
+                foreach (var ingredient in firstIngredients)
+                {
+                    allIngredients[ingredient] = allergen.Key;
+
+                }
+            }
+
+            var results = 0;
+            foreach (var kvp in allIngredients.Where(i => i.Value == null))
+            {
+                foreach (var food in foods)
+                {
+                    if (food.Ingredients.Contains(kvp.Key))
+                        results += 1;
+                }
+            }
+
+            return results.ToString();
         }
 
         public override string Part2()
         {
-            throw new NotImplementedException();
+            var lines = input.Split(Environment.NewLine);
+            var foods = new List<Food>();
+            var allIngredients = new Dictionary<string, List<string>>();
+            var allAllergens = new Dictionary<string, List<Food>>();
+            foreach (var line in lines)
+            {
+                var parts = Regex.Match(line, "(.*) \\(contains (.*)\\)");
+                var ingredients = parts.Groups[1].Value.Split(" ");
+                var allergens = parts.Groups[2].Value.Split(", ");
+                foreach (var ingredient in ingredients)
+                {
+                    allIngredients[ingredient] = new List<string>();
+                }
+
+                var food = new Food(new HashSet<string>(allergens), new HashSet<string>(ingredients));
+                foreach (var allergen in allergens)
+                {
+                    if (allAllergens.ContainsKey(allergen))
+                        allAllergens[allergen].Add(food);
+                    else
+                        allAllergens[allergen] = new List<Food>() { food };
+                }
+
+                foods.Add(food);
+            }
+
+            var allergensToCheck = new Queue<string>(allAllergens.Keys);
+            while (allergensToCheck.Count > 0)
+            {
+                var allergen = allergensToCheck.Dequeue();
+                var foodsWithAllergen = allAllergens[allergen];
+                var firstIngredients =
+                    new HashSet<string>(foodsWithAllergen[0].Ingredients.Where(i => allIngredients[i].Count == 0));
+                foreach (var food in foodsWithAllergen.Skip(1))
+                {
+                    firstIngredients.IntersectWith(food.Ingredients.Where(i => allIngredients[i].Count == 0));
+                }
+
+                if (firstIngredients.Count != 1)
+                {
+                    allergensToCheck.Enqueue(allergen);
+                    continue;
+                }
+
+                allIngredients[firstIngredients.First()].Add(allergen);
+            }
+
+
+            var results = allIngredients
+                    .Where(i => i.Value.Count > 0)
+                    .OrderBy(n => n.Value[0])
+                    .Select(i => i.Key)
+                    .ToArray();
+
+            return string.Join(",", results);
         }
     }
 }
--- Tests/Day21Tests.cs
+++ Tests/Day21Tests.cs
@@ -6,17 +6,23 @@ namespace Tests
 {
     public class Day21Tests : DayTest<Day21>
     {
-        [Input(@"input", "expected")]
+        [Input(@"mxmxvkd kfcds sqjhc nhms (contains dairy, fish)
+trh fvjkl sbzzf mxmxvkd (contains dairy)
+sqjhc fvjkl (contains soy)
+sqjhc mxmxvkd sbzzf (contains fish)", "5")]
         public void Part1(string input, string output)
         {
-            var result = Sut(input.Replace(",", "\r\n")).Part1();
+            var result = Sut(input).Part1();
             result.ShouldBe(output);
         }
 
-        [Input(@"input", "expected")]
+        [Input(@"mxmxvkd kfcds sqjhc nhms (contains dairy, fish)
+trh fvjkl sbzzf mxmxvkd (contains dairy)
+sqjhc fvjkl (contains soy)
+sqjhc mxmxvkd sbzzf (contains fish)", "mxmxvkd,sqjhc,fvjkl")]
         public void Part2(string input, string output)
         {
-            var result = Sut(input.Replace(",", "\r\n")).Part2();
+            var result = Sut(input).Part2();
             result.ShouldBe(output);
         }
     }
